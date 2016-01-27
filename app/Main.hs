544e38b5e57e98702cb84ece3ed01ee366ccea00
Michael Trotter <spicydonuts@gmail.com>
2016-01-27 01:26:24 -0700

    2-applicative

--- app/Main.hs
+++ app/Main.hs
@@ -7,17 +7,8 @@ data ThreeNumbers = ThreeNumbers Int Int Int deriving (Show)
 main :: IO ()
 main = do
   putStrLn "Gimme 3 numbers!"
-  n1 <- getLine
-  n2 <- getLine
-  n3 <- getLine
-  let nums =
-        case readMaybe n1 of
-          Nothing -> Nothing
-          Just n1' ->
-            case readMaybe n2 of
-              Nothing -> Nothing
-              Just n2' ->
-                case readMaybe n3 of
-                  Nothing -> Nothing
-                  Just n3' -> Just (ThreeNumbers n1' n2' n3')
+  n1 <- readMaybe <$> getLine
+  n2 <- readMaybe <$> getLine
+  n3 <- readMaybe <$> getLine
+  let nums = ThreeNumbers <$> n1 <*> n2 <*> n3
   print nums
