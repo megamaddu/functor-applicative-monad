02f1cb11f800cdfbb369970494f11509038c00fa
Michael Trotter <spicydonuts@gmail.com>
2016-01-27 01:28:10 -0700

    4-validation

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,21 @@
 module Main where
 
+import Data.List
 import Text.Read
 
 data ThreeNumbers = ThreeNumbers Int Int Int deriving (Show)
+
+data Validation a = Failure [(String, String)]
+                  | Success a
+
+(<?>) :: Maybe a -> (String, String) -> Validation a
+Just a  <?> _ = Success a
+Nothing <?> e = Failure [e]
+
+instance Show a => Show (Validation a) where
+  show (Success a) = "Success:\n" ++ show a
+  show (Failure e) = intercalate "\n- " ("It broke:" : (showError <$> e))
+    where showError (a, b) = a ++ ": " ++ b
 
 main :: IO ()
 main = do
@@ -10,9 +23,5 @@ main = do
   n1 <- readMaybe <$> getLine
   n2 <- readMaybe <$> getLine
   n3 <- readMaybe <$> getLine
-  let nums = do
-        n1' <- n1
-        n2' <- n2
-        n3' <- n3
-        return (ThreeNumbers n1' n2' n3')
+  let nums = ThreeNumbers <$> n1 <*> n2 <*> n3
   print nums
