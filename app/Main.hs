3f43f6bf5a9d78b4d85b4e03d514d872290be339
Michael Trotter <spicydonuts@gmail.com>
2016-01-27 01:30:34 -0700

    6-validation-monad

--- app/Main.hs
+++ app/Main.hs
@@ -29,13 +29,19 @@ instance Applicative Validation where
   Failure e <*> Failure d = Failure (e ++ d)
   Failure e <*> _         = Failure e
 
+instance Monad Validation where
+  Failure e >>= _ = Failure e
+  Success a >>= f = f a
+
 main :: IO ()
 main = do
   putStrLn "Gimme 3 numbers!"
   n1 <- readMaybe <$> getLine
   n2 <- readMaybe <$> getLine
   n3 <- readMaybe <$> getLine
-  let nums = ThreeNumbers <$> n1 <?> ("n1", "that's not a number!")
-                          <*> n2 <?> ("n2", "neither is that!!")
-                          <*> n3 <?> ("n3", "really?!")
+  let nums = do
+        n1' <- n1 <?> ("n1", "that's not a number!")
+        n2' <- n2 <?> ("n2", "neither is that!!")
+        n3' <- n3 <?> ("n3", "really?!")
+        return (ThreeNumbers n1' n2' n3')
   print nums
